04559faf0754355988de151eeaeced288bec93b8
GabrielSilvaD3v <123119325+GabrielD3Vsilva@users.noreply.github.com>
2024-07-12 17:28:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,14 @@ app.use(cors({
     methods: ["GET", "POST"]
 }));
 
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
+
 const users = [];
 const messages = [];
 
